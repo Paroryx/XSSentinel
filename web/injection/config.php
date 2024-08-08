b546772790eb63772bf304d5e2d8b8a0c27d1238
paroryx <paroryx@latitude-D520.res.v6.highway.a1.net>
2024-08-08 16:04:15 +0200

    added a mysql database `honeypot` with the table `HoneyLog`

---
+++ web/injection/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = 'localhost';
+$db = 'honeypot';
+$user = 'paroryx';
+$password = 'secretPassword33!';
+
+?>
--- web/injection/injection_attacks.php
+++ web/injection/injection_attacks.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
-<html>
+<?php
+require 'config.php';
+?>
 <head>
+<style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
 <title>Injection Attacks</title>
 </head>
 <body>
@@ -12,7 +19,52 @@ echo "<br>";
 $default_consts = "directory:  ".__DIR__."\nfile: ".__FILE__."\nLine nr: ".__line__."\nThe default constants are case insensitive! :)";
 echo nl2br($default_consts);
 echo "The time is " . date("h:i:sa");
+//echo phpinfo();
+echo "<br><h3>Database Connection: </h3><br>";
+?>
+<?php
+function createTable($rows){
+	$html_table = '<table style="width:100%">';
+
+	$html_table.="<tr>";
+        foreach ($rows[0] as $type => $data){
+           $html_table.="<th>".$type."</th>";
+        }
+	$html_table.="</tr>";
+
+	foreach($rows as $row){
+           $html_table.="<tr>";
+           foreach($row as $type => $data){
+              $html_table.="<td>".$data."</td>";
+           }
+           $html_table.="</tr>";
+        }
+
+	$html_table.="</table>";
+
+	return $html_table;
+}
+?>
+<?php
+
+$dsn = "mysql:host=localhost;dbname=honeypot;charset=UTF8";
+try{
+        $pdo = new PDO($dsn, $user, $password);
+
+        if($pdo) {
+                echo "succesfully connected to the $db database! :)<br>";
+		echo "<h4><code>HoneyLog</code> from <code>$db</code> Database</h4>";
+		$sql = 'SELECT * FROM HoneyLog';
+		$statement = $pdo->query($sql);
+		$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+        }
+} catch (PDOException $e){
+        echo $e->getMessage();
+}
+
+$table = createTable($rows);
+echo $table;
+
 ?>
 </body>
 </html>
-
