98b97cc8da8314099db3a61f1a8072027ed60905
paroryx <paroryx@latitude-D520.res.v6.highway.a1.net>
2024-08-08 02:59:14 +0200

    still playing with php

--- web/injection_attacks.php
+++ web/injection_attacks.php
@@ -3,12 +3,13 @@
 <body>
  
 <?php
-echo "PHP Version: ".phpversion()."<br>";
 $text = "World";
 echo "My first PHP script!";
 echo "<h1>Hello $text!</h1>";
+echo "<br>";
+$default_consts = "directory:  ".__DIR__."\nfile: ".__FILE__."\nLine nr: ".__line__."\nThe default constants are case sensitive! :)";
+echo nl2br($default_consts);
 ?>
-
 </body>
 </html>
 
