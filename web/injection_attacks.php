6dce7b5e46c908a1bc265f3bca5fff874dfd485a
paroryx <paroryx@latitude-D520.res.v6.highway.a1.net>
2024-08-08 03:02:09 +0200

    case INsensitive

--- web/injection_attacks.php
+++ web/injection_attacks.php
@@ -7,7 +7,7 @@ $text = "World";
 echo "My first PHP script!";
 echo "<h1>Hello $text!</h1>";
 echo "<br>";
-$default_consts = "directory:  ".__DIR__."\nfile: ".__FILE__."\nLine nr: ".__line__."\nThe default constants are case sensitive! :)";
+$default_consts = "directory:  ".__DIR__."\nfile: ".__FILE__."\nLine nr: ".__line__."\nThe default constants are case insensitive! :)";
 echo nl2br($default_consts);
 ?>
 </body>
