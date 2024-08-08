0dcedc1c997d1d88792dd333388b57c4866cab4d
paroryx <paroryx@latitude-D520.res.v6.highway.a1.net>
2024-08-08 03:28:24 +0200

    final commit for this night :)

--- web/injection_attacks.php
+++ web/injection_attacks.php
@@ -9,6 +9,7 @@ echo "<h1>Hello $text!</h1>";
 echo "<br>";
 $default_consts = "directory:  ".__DIR__."\nfile: ".__FILE__."\nLine nr: ".__line__."\nThe default constants are case insensitive! :)";
 echo nl2br($default_consts);
+echo "The time is " . date("h:i:sa");
 ?>
 </body>
 </html>
